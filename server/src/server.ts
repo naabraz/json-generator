eb4f4a72ae445cb8b2576c9859efee89b1a4d7cf
Natalia Braz <naabraz@gmail.com>
2023-04-07 20:46:05 -0300

    feat: add post route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/time', (_, res: Response) => {
   res.json({ date: Date.now() });
+});
+
+app.post('/create', (req: Request, res: Response) => {
+  const data = req.body;
+
+  res.send(`Data received: ${JSON.stringify(data)}`);
 });
 
 app.listen(port, () => {
